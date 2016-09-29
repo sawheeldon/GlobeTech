d1b6cefdc76d826b199ee95e962af5a1ec575eac
Simon <simon@jampot.design>
2016-09-29 21:09:49 +0000

    working on main page with logos

--- build/globetech.1.0.0.js
+++ build/globetech.1.0.0.js
@@ -49,39 +49,52 @@
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(34);
 	
-	// storing the logos in a global variable
+	// array to store the images
 	
-	var techLogos = {
-	  mashable: '../images/mashable.png',
-	  ars: '../images/Ars.svg.png',
-	  recode: '../images/recode.svg.png',
-	  verge: '../images/verge.png'
+	var techLogos = ['../images/verge.png', '../images/mashable.png', '../images/Recode.svg.png', '../images/TechRadar.png'];
+	
+	// loop for logo
+	
+	var FrontLogos = function FrontLogos() {
+	    var outPut = "";
+	    for (var i = 0; i < techLogos.length; i++) {
+	        setTimeout(function () {
+	            techLogos(i);
+	        }, 100);
+	        outPut = techLogos[i];
+	    }
 	};
 	
-	function coinToss() {
-	  // Randomly return either 'heads' or 'tails'.
-	  return Math.random() < 0.5 ? 'heads' : 'tails';
-	}
+	// function coinToss () {
+	//   // Randomly return either 'heads' or 'tails'.
+	//   return Math.random() < 0.5 ? 'heads' : 'tails';
+	// }
 	
-	var imageLogo = React.createElement('img', { src: techLogos[coinToss() == 'heads' ? 'mashable' : 'ars'] });
+	// var imageLogo = <img src={techLogos[coinToss()=='heads' ? 'mashable' : 'ars']} />;
+	
 	
 	//test to make sure React is working
 	
 	var Test = React.createClass({
-	  displayName: 'Test',
+	    displayName: 'Test',
 	
-	  render: function render() {
-	    return React.createElement(
-	      'h1',
-	      null,
-	      'Hello world, testing the React Link'
-	    );
-	  }
+	    render: function render() {
+	        return React.createElement(
+	            'div',
+	            null,
+	            React.createElement(
+	                'h1',
+	                null,
+	                'Hello world, testing the React Link'
+	            ),
+	            React.createElement('img', { height: '100', src: techLogos[FrontLogos()] })
+	        );
+	    }
 	});
 	
 	document.addEventListener('DOMContentLoaded', function () {
-	  ReactDOM.render(React.createElement(Test, null), document.getElementById('app'));
-	  ReactDOM.render(imageLogo, document.getElementById('logo'));
+	    ReactDOM.render(React.createElement(Test, null), document.getElementById('app'));
+	    // ReactDOM.render(imageLogo, document.getElementById('logo'));
 	});
 
 /***/ },
--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-// storing the logos in a global variable
+// array to store the images
 
-var techLogos = {
-  mashable:'../images/mashable.png',
-  ars:'../images/Ars.svg.png',
-  recode:'../images/recode.svg.png',
-  verge:'../images/verge.png',
+var techLogos = ['../images/verge.png', '../images/mashable.png', '../images/Recode.svg.png', '../images/TechRadar.png'];
+
+// loop for logo
+
+var FrontLogos = function() {
+    var outPut = "";
+    for (var i = 0; i<techLogos.length; i++) {
+        setTimeout(function(){techLogos(i)},100);
+        outPut = techLogos[i];
+    }
 };
 
-function coinToss () {
-  // Randomly return either 'heads' or 'tails'.
-  return Math.random() < 0.5 ? 'heads' : 'tails';
-}
 
-var imageLogo = <img src={techLogos[coinToss()=='heads' ? 'mashable' : 'ars']} />;
+
+
+
+// function coinToss () {
+//   // Randomly return either 'heads' or 'tails'.
+//   return Math.random() < 0.5 ? 'heads' : 'tails';
+// }
+
+// var imageLogo = <img src={techLogos[coinToss()=='heads' ? 'mashable' : 'ars']} />;
 
 
 //test to make sure React is working
 
 var Test = React.createClass({
   render: function () {
-    return <h1>Hello world, testing the React Link</h1>;
+    return ( 
+    <div>
+    <h1>Hello world, testing the React Link</h1>
+    <img height="100" src={techLogos[FrontLogos()]} />
+    </div>
+    );
   }
 });
 
@@ -33,5 +47,5 @@ var Test = React.createClass({
 
 document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(<Test />, document.getElementById('app'));
-    ReactDOM.render(imageLogo, document.getElementById('logo'));
+    // ReactDOM.render(imageLogo, document.getElementById('logo'));
 });
